22eeb5b1cbcbd91b892ab7996e4c6a7e62e2b275
Artur Daveian <artur.daveian@akvelon.com>
2016-04-19 17:19:16 +0300

    cleaned

--- dev/contacts/components/contact-list.component.ts
+++ dev/contacts/components/contact-list.component.ts
@@ -11,31 +11,6 @@ import {Contact} from "./../contact";
   providers:[ContactService],
   styleUrls:["../src/css/app.css"]
 })
-// Without Lifecycle hooks - call thru constructor
-/*
-export class ContactListComponent{
-  public contacts : Contact[];
-
-  public selectedContact = {};
-
-  public onNameClick(contact){
-        this.selectedContact = contact;
-  }
-  //info: if you want to pre-select first element from contact list then
-  //      you can define constructor on this class and initialize defaults.
-  //this is same as
-  // private contactService : ContactService
-  // this.contactService = contactService;
-  constructor(private _contactService: ContactService){
-      this.getContacts();
-  }
-
-  // Call service now
-  getContacts(){
-    this._contactService.getContacts().then((contacts:Contact[])=>this.contacts = contacts);
-  }
-}
-*/
 
 
 // With Lifecycle hooks
